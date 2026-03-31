543ab34e7c6afdfb993e95b47eba1226e678308a
Cloesick <nicolas.cloet@hotmail.be>
2026-03-31 12:21:21 +0200

    chore: add basePath for gateway routing

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/favicon.ico",
-        destination: "/icon.svg",
-        permanent: true,
-      },
-    ];
-  },
-  images: {
-    dangerouslyAllowSVG: true,
-    contentDispositionType: "attachment",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+	distDir: ".next-fashion",
+	basePath: "/fashion",
+	async redirects() {
+		return [
+			{
+				source: "/favicon.ico",
+				destination: "/icon.svg",
+				permanent: true,
+			},
+		];
+	},
+	images: {
+		dangerouslyAllowSVG: true,
+		contentDispositionType: "attachment",
+		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
