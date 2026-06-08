53305cff08be2317a69dc3fa966b69bcc062e0e6
Nicolas <93992407+Cloesick@users.noreply.github.com>
2026-06-08 02:15:30 +0200

    Refactor next.config.ts for output and trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	distDir: ".next-fashion",
-	basePath: "/fashion",
-	async redirects() {
-		return [
-			{
-				source: "/favicon.ico",
-				destination: "/icon.webp",
-				permanent: true,
-			},
-		];
-	},
-	images: {
-		dangerouslyAllowSVG: true,
-		contentDispositionType: "attachment",
-		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "**",
-			},
-		],
-	},
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
